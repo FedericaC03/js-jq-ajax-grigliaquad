5900a295b61173b8aeeb151d64a6b34b625de2ca
FedericaC03 <fdrk03@hotmail.it>
2020-09-16 15:54:50 +0200

    html+css

--- script/script.js
+++ script/script.js
@@ -1,1 +1,13 @@
-alert("ciao");
+// alert("ciao");
+// $.ajax(
+// {
+// url: "http://www.boolean.careers/api/random/boolean",
+// method: "GET",
+// success: function (data, stato) {
+// $("#risultati").html(data);
+// },
+// error: function (richiesta, stato, errori) {
+// alert("E' avvenuto un errore. " + errore);
+// }
+// }
+// );
