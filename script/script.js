ad867f39cb1a19dfdd0d1106b594d9a073dcf67e
FedericaC03 <fdrk03@hotmail.it>
2020-09-16 17:24:59 +0200

    fine esercizio

--- script/script.js
+++ script/script.js
@@ -1,13 +1,23 @@
-// alert("ciao");
-// $.ajax(
-// {
-// url: "http://www.boolean.careers/api/random/boolean",
-// method: "GET",
-// success: function (data, stato) {
-// $("#risultati").html(data);
-// },
-// error: function (richiesta, stato, errori) {
-// alert("E' avvenuto un errore. " + errore);
-// }
-// }
-// );
+$(document).ready(function() {
+  $.ajax(
+  {
+  "url": "https://flynn.boolean.careers/exercises/api/random/int",
+  "method": "GET",
+  "success": function (data, stato) {
+    console.log(data.response);
+    $(".square" ).click(function() {
+      if (data.response > 5) {
+        $(this).addClass("active-green");
+        $(this).text(data.response);
+      } else {
+        $(this).addClass("active-yellow");
+        $(this).text(data.response);
+      };
+    });
+  },
+  error: function (richiesta, stato, errori) {
+  alert("errore");
+  }
+  }
+  );
+});
